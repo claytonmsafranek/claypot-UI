21e596199782debd9a623d0f26b58caf3024f3a4
Clayton Safranek <claytonsafranek@gmail.com>
2024-03-15 20:12:58 -0500

    define types

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
     selector: 'app-root',
@@ -19,15 +21,15 @@ import { CommonModule } from '@angular/common';
 
 export class AppComponent implements OnInit {
   title = 'Claypots';
-  products: any[] = []; // TODO: come back and interface this
+  products: Product[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response: any) => this.products = response.data,  // what to do next
-      error: error => console.log(error),       // what to do if there is an error
-      complete: () => {                         // what to do when the request completes
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data,  // what to do next
+      error: error => console.log(error),               // what to do if there is an error
+      complete: () => {                                 // what to do when the request completes
         console.log('request completed');
         console.log('extra statement');
       }
