f2bf15a33b7b843b43d6451a44fd1b4292def8bf
Clayton Safranek <claytonsafranek@gmail.com>
2024-03-15 19:39:47 -0500

    make request for products

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, NavBarComponent]
+    imports: [
+      RouterOutlet,
+      NavBarComponent,
+      HttpClientModule,
+      CommonModule
+    ]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Claypots';
+  products: any[] = []; // TODO: come back and interface this
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response: any) => this.products = response.data,  // what to do next
+      error: error => console.log(error),       // what to do if there is an error
+      complete: () => {                         // what to do when the request completes
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    });
+  }
 }
